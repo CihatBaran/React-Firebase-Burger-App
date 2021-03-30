d049cac05fbf68db42920362f92bde3e768b83dc
Cihat Baran <cihatbarann@gmail.com>
2021-03-30 17:29:19 -0400

    Active Class for Navbar defined with NavLink

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React from 'react';
+import classes from './Order.module.css';
 
 const Order = (props) => {
-  return <div></div>;
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: Salad</p>
+      <p>
+        Price: <strong>4.5 $</strong>
+      </p>
+    </div>
+  );
 };
 export default Order;
